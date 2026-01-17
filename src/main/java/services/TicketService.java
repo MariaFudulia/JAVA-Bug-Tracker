0656e5f861b251243d051e5e466f1ab385c30640
MariaFudulia <maria.fudulia@stud.acs.upb.ro>
2026-01-18 00:40:43 +0200

    Added TicketService to manage ticket related commands.

---
+++ src/main/java/services/TicketService.java
@@ -0,0 +1,95 @@
+package services;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import context.AppContext;
+import database.TicketDatabase;
+import tickets.*;
+import users.User;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+
+public class TicketService {
+    private final TicketDatabase ticketDatabase;
+
+    public TicketService(final TicketDatabase ticketDatabase) {
+        this.ticketDatabase = ticketDatabase;
+    }
+
+    public Ticket createTicket(AppContext appContext) {
+        ObjectMapper mapper = appContext.getMapper();
+        ObjectNode node = mapper.createObjectNode();
+        JsonNode input = appContext.getInput();
+        JsonNode params =  input.get("params");
+        TicketType type = TicketType.valueOf(params.get("type").asText());
+        String reporter = params.get("reportedBy").asText();
+
+        if (reporter.isEmpty() && type != TicketType.BUG) {
+            return null;
+        }
+
+        TicketBuilder builder = TicketBuilderFactory.create(type);
+
+        String timestamp = input.get("timestamp").asText();
+        LocalDate createdAt = LocalDate.parse(timestamp);
+
+        builder.id(ticketDatabase.getTotalTickets())
+                .title(params.get("title").asText())
+                .businessPriority(TicketPriority.valueOf(params
+                        .get("businessPriority").asText()))
+                .expertiseArea(Expertise.valueOf(params
+                        .get("expertiseArea").asText()))
+                .reportedBy(reporter).status(TicketStatus.OPEN)
+                .createdAt(createdAt);
+
+        if (params.has("description"))
+            builder.description(params.get("description").asText());
+
+        if (params.get("reportedBy").asText().equals(""))
+            builder.businessPriority(TicketPriority.LOW);
+
+        switch (type) {
+            case BUG -> {
+                ((BugTicketBuilder) builder).frequency(BugFrequency
+                                .valueOf(params.get("frequency").asText()))
+                        .severity(BugSeverity.valueOf(params.get("severity").asText()));
+            }
+
+            case FEATURE_REQUEST -> {
+                ((FeatureRequestTicketBuilder) builder).businessValue(Impact.valueOf(params.get("businessValue")
+                        .asText())).customerDemand(Demand.valueOf(params.get("customerDemand").asText()));
+            }
+
+            case UI_FEEDBACK -> {
+                ((UiFeedbackTicketBuilder) builder).uiElementId(params.get("uiElementId").asText())
+                        .usabilityScore(params.get("usabilityScore").asInt());
+            }
+        }
+
+        Ticket ticket = builder.build();
+        ticketDatabase.addTicket(ticket);
+        ticketDatabase.incTickets();
+        return ticket;
+    }
+
+    public List<Ticket> getVisibleTicketsForUser(User user) {
+        Map<Integer, Ticket> tickets = ticketDatabase.getTickets();
+        List<Ticket> ticketList = user.getVisibleTickets(tickets);
+
+        return sort(ticketList);
+    }
+
+    private List<Ticket> sort(List<Ticket> ticketList) {
+        return ticketList.stream().sorted(
+                Comparator.comparing(Ticket::getCreatedAt).thenComparing(Ticket::getId)).toList();
+    }
+
+    public Ticket getTicketById(int ticketId) {
+        return ticketDatabase.getTicket(ticketId);
+    }
+}
